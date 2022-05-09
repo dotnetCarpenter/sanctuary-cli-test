8e66eb4ce2f7c8cef26f82e9463ffc2b06a6e356
dotnetCarpenter <jon.ronnenberg@gmail.com>
2022-05-09 13:28:13 +0200

    Use Dict as Model and validation seems to work

--- elm-required-fields/src/Main.elm
+++ elm-required-fields/src/Main.elm
@@ -11,7 +11,8 @@ module Main exposing (..)
 
 import Browser
 import Dict exposing (Dict)
-import Html exposing (Html, div, input, text)
+import List
+import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onInput)
 
@@ -20,17 +21,8 @@ import Html.Events exposing (onInput)
 -- MAIN
 
 
-main : Program () Model Msg
+main : Program () Config Msg
 main = Browser.sandbox { init = init, update = update, view = view }
-
-
-stringFromBool : Bool -> String
-stringFromBool value =
-    if value then
-        ""
-
-    else
-        "not"
 
 
 
@@ -39,27 +31,88 @@ type Field
     = Any String
     | RequiredField String String
 
--- type alias Model =
---     { user : Field
---     , password : Field
---     , arbitrary : Field
---     , isValid : Bool
---     }
-type alias Model =
-    {
-        fields: Dict String Field
-        , isValid : Bool
-    }
+
+type alias Config = Dict String Field
 
 
-init : Model
-init = { fields = Dict.fromList
-            [ ("user", RequiredField "password" "")
-            , ("password", RequiredField "user" "")
-            , ("arbitrary", Any "foo")
-            ]
-        , isValid = False
-        }
+init : Config
+init = Dict.fromList
+        [ ("user", RequiredField "password" "")
+        , ("password", RequiredField "user" "")
+        , ("arbitrary", Any "foo")
+        ]
+
+
+validate : Config -> Bool
+validate config = {- key is only needed if we need to communicate what was invalid -}
+    let validateField_ = validateField config
+    in
+        Dict.foldl (\_ field output -> output && validateField_ field) True config
+
+
+validateField : Config -> Field -> Bool
+validateField config field =
+    case field of
+        Any _ ->
+            True
+
+        RequiredField fieldName _ ->
+            config
+                |> Dict.get fieldName
+                |> Maybe.map isFieldValid
+                |> Maybe.withDefault False
+
+            -- config
+            --     |> Dict.toList
+            --     -- List ( String, Field )
+
+            --     |> List.filter (\(_, value) ->
+            --         case value of
+            --             RequiredField _ _ -> True
+            --             Any _ -> False)
+            --     -- List ( String, RequiredField )
+
+            --     |> List.all (\(key, value) ->
+            --         key == fieldName && value /= RequiredField fieldName "")
+            --     -- Bool
+
+isFieldValid : Field -> Bool
+isFieldValid field =
+    case field of
+        RequiredField _ value -> value /= ""
+        Any _ -> False
+
+
+-- UPDATE
+
+
+type Msg
+    = UpdateUser String
+    | UpdatePassword String
+
+
+update : Msg -> Config -> Config
+update _ config =
+    config
+    -- case msg of
+    --     UpdateUser name ->
+    --         { model | user = RequiredField "password" name }
+
+    --     UpdatePassword pw ->
+    --         { model | password = RequiredField "user" pw }
+
+
+
+-- VIEW
+
+
+boolToString : Bool -> String
+boolToString value =
+    if value then
+        ""
+
+    else
+        "not"
 
 
 fieldToString : Field -> String
@@ -72,49 +125,42 @@ fieldToString field =
             value
 
 
-modelToString : Model -> Html msg
-modelToString model =
-    text (fieldToString model.user ++ fieldToString model.password ++ " is " ++ stringFromBool model.isValid ++ " valid")
+configToHtmlList : Config -> Html msg
+configToHtmlList config =
+    ol
+    []
+    (Dict.foldl
+        (\key field output ->
+            li
+            []
+            [ text ("Key: " ++ key ++ " Value: " ++ fieldToString field) ] :: output)
+        []
+        config)
 
 
-validate : Model -> Bool
-validate model =
-    List.map2 .user .password model
---    case field of
---        Any ->
---            True
---
---        RequiredField required _ ->
---            Model
-
-
-
--- UPDATE
-
-
-type Msg
-    = UpdateUser String
-    | UpdatePassword String
-
-
-update : Msg -> Model -> Model
-update msg model =
-    case msg of
-        UpdateUser name ->
-            { model | user = RequiredField "password" name }
-
-        UpdatePassword pw ->
-            { model | password = RequiredField "user" pw }
-
-
-
--- VIEW
-
-
-view : Model -> Html Msg
-view model =
+view : Config -> Html Msg
+view config =
     div []
-        [ input [ placeholder "User", value (fieldToString model.user), onInput UpdateUser ] []
-        , input [ placeholder "Password", value (fieldToString model.password), onInput UpdatePassword ] []
-        , div [ attribute "style" "color:white;" ] [ modelToString model ]
-        ]
+        (
+            (viewInputFields config)
+            ++
+            [
+                p [] [ text ("The configuration is " ++ (boolToString (validate config)) ++ " valid.") ]
+            ,   configToHtmlList config
+            ]
+        )
+
+
+viewInputFields : Config -> List (Html msg)
+viewInputFields config =
+    Dict.foldl
+        (\key field fields -> viewInputField key field :: fields)
+        []
+        config
+
+
+viewInputField : String -> Field -> Html msg
+viewInputField key field =
+    input
+        [ placeholder key, value (fieldToString field) ]
+        [  ]
